0c6032b0aa2db5cadc3f75f8dd359e5e859e47bb
Gusytavofelix <167647462+Gusytavofelix@users.noreply.github.com>
2024-11-12 10:03:33 -0300

    Add files via upload

---
+++ Alura-4BIM-Parte2/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Aventura',
+    'Qual o nome do protagonista de The Legend of Zelda?',
+    'O protagonista é Link.'
+);
+
+criaCartao(
+    'RPG',
+    'Qual o nível máximo em World of Warcraft?',
+    'O nível máximo depende da expansão atual.'
+);
+
+criaCartao(
+    'Ação',
+    'Em que jogo encontramos a cidade de Vice City?',
+    'Vice City aparece em GTA: Vice City.'
+);
+
+criaCartao(
+    'FPS',
+    'Qual é o nome do herói de Call of Duty: Modern Warfare?',
+    'O protagonista é o Capitão Price.'
+);
+
+criaCartao(
+    'Estratégia',
+    'Em que ano foi lançado o Age of Empires II?',
+    'O jogo foi lançado em 1999.'
+);
